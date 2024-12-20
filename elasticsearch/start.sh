e9441e511ff21e20e424b44913490ed65f1cfcac
mikeaaaaaa <932379959@qq.com>
2024-12-20 02:48:00 +0000

    add redis

--- elasticsearch/start.sh
+++ elasticsearch/start.sh
@@ -3,8 +3,16 @@ sudo rm -rf ./data/
 mkdir -p ./data/es/{es01,es02,es03}/data
 mkdir -p ./data/es/{es01,es02,es03}/log
 mkdir -p ./data/es/plugins/elasticsearch-analysis-ik
+mkdir -p ./data/es/plugins/elasticsearch-analysis-pinyin
+# install 分词 插件
 wget https://ghp.ci/https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.2.0/elasticsearch-analysis-ik-7.2.0.zip -O t.zip
 unzip t.zip -d ./data/es/plugins/elasticsearch-analysis-ik
+
+# install 拼音插件
+wget https://release.infinilabs.com/analysis-pinyin/stable/elasticsearch-analysis-pinyin-7.2.0.zip -O t.zip
+unzip t.zip -d ./data/es/plugins/elasticsearch-analysis-pinyin
+
+
 
 
 sudo chmod -R 777 ./data 
